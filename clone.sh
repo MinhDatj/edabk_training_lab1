25f0f16f168bb7f0509009a7a7d64fef4661e024
MinhDatj <nlmdat1204@gmail.com>
2024-12-22 09:47:32 +0700

    adding guidance into clone.sh

--- clone.sh
+++ clone.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
 #SOURCE=$1
-git clone $1
+git clone "$1" && cd "$(basename "$1" .git)"
 
-REPO_NAME=$(basename "$1" .git)
-
-if [ -d "$REPO_NAME" ]; then
-	echo "successfully cloned into $REPO_NAME"
-	cd "$REPO_NAME" || exit
-	echo "Now in directory: $(pwd)"
-else 
-	echo "Failed to clone reposistory."
-	exit 1
-fi
+#run and automatically cd into your repo by ". ./clone.sh <repo_name>"
