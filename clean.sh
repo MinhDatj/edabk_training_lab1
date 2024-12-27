3fad97f75b23e275c5a828fbde6228a52bf44193
MinhDatj <nlmdat1204@gmail.com>
2024-12-27 09:14:37 +0700

    Modified clean.sh

--- clean.sh
+++ clean.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-file="LinkList.exe"
-
-if [ -f "$file" ]; then
-	rm "$file"
+file1="LinkList.exe"
+file2="name.txt"
+if [ -f "$file1" ]; then
+	rm "$file1"
+fi
+if [ -f "$file2" ]; then
+	rm "$file2"
 fi
 	
